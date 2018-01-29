338454bafcd7c4b5a2e55183dd7e876ca0e56fb0
Stefan Tórz <storz@interia.eu>
2018-01-29 20:20:21 +0100

    change files

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-function getQuote() {
+/*function getQuote() {
     $.getJSON(quoteUrl, createTweet);
-}
+}*/
+ $(function(){
+function getQuote() {
+      $.getJSON(prefix + quoteUrl, createTweet);
+      $.ajaxSetup({ cache: false });
+  }
 function createTweet(input) {
     var data = input[0];
 
     var quoteText = $(data.content).text().trim();
     var quoteAuthor = data.title;
-
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
-}
-var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-if (tweetText.length > 140) {
+    if (tweetText.length > 140) {
     getQuote();
-} else {
+    } else {
     var tweet = tweetLink + encodeURIComponent(tweetText);
     $('.quote').text(quoteText);
     $('.author').text("Author: " + quoteAuthor);
     $('.tweet').attr('href', tweet);
 }
+}
+
+
 $(document).ready(function() {
     getQuote();
     $('.trigger').click(function() {
         getQuote();
     })
+});
 });
